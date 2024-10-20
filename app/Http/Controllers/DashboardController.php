aab739c458adb050299dba6275e1d5e13d425210
Hizkia Reppi <hizkiajefren0501@gmail.com>
2024-10-20 21:57:10 +0800

    feat(dashboard): add total data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use App\Models\IKU7;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Gate;
@@ -17,6 +20,20 @@ class DashboardController extends Controller
             abort(403);
         }
 
-        return view('dashboard.index');
+        $totalDepartmentsWithAccounts = User::has('prodi')->count();
+        $totalCourses = Course::count();
+        $totalCoursesReported = $this->getTotalCoursesReported();
+        $totalCoursesNotReported = $this->getTotalCoursesNotReported();
+        return view('dashboard.index', compact('totalCourses', 'totalDepartmentsWithAccounts', 'totalCoursesReported', 'totalCoursesNotReported'));
+    }
+
+    private function getTotalCoursesReported()
+    {
+        return IKU7::whereNotNull('score_case_method')->whereNotNull('score_project_based')->whereNotNull('score_cognitive_task')->whereNotNull('score_cognitive_quiz')->whereNotNull('score_cognitive_uts')->whereNotNull('score_cognitive_uas')->whereNotNull('description_case_method')->whereNotNull('description_project_based')->whereNotNull('description_cognitive_task')->whereNotNull('description_cognitive_quiz')->whereNotNull('description_cognitive_uts')->whereNotNull('description_cognitive_uas')->whereNotNull('file_rps')->count();
+    }
+
+    private function getTotalCoursesNotReported()
+    {
+        return IKU7::whereNull('score_case_method')->orWhereNull('score_project_based')->orWhereNull('score_cognitive_task')->orWhereNull('score_cognitive_quiz')->orWhereNull('score_cognitive_uts')->orWhereNull('score_cognitive_uas')->orWhereNull('description_case_method')->orWhereNull('description_project_based')->orWhereNull('description_cognitive_task')->orWhereNull('description_cognitive_quiz')->orWhereNull('description_cognitive_uts')->orWhereNull('description_cognitive_uas')->orWhereNull('file_rps')->count();
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,5 +3,98 @@
         Dashboard
     </x-slot>
 
-    <div class="card px-4 pt-4 pb-3 mb-4"></div>
+    <div class="card px-4 pt-4 pb-3 mb-4">
+        <div class="row">
+            <div class="col-md-3 col-6 mb-2">
+                <div class="card p-1">
+                    <div class="card-body">
+                        <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="d-flex align-items-center">
+                                <i class="menu-icon tf-icons fa-solid fa-chalkboard-user"></i>
+                                <span class="fw-medium d-block mb-1">Total Akun Prodi</span>
+                            </div>
+                            <div class="dropdown">
+                                <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    <i class="bx bx-dots-vertical-rounded"></i>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                                    <a class="dropdown-item" href="{{ route('dashboard.admin-prodi.index') }}">Lihat
+                                        Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                        <h3 class="card-title mb-0 mt-3">{{ $totalDepartmentsWithAccounts }}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-6 mb-2">
+                <div class="card p-1">
+                    <div class="card-body">
+                        <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="d-flex align-items-center">
+                                <i class="menu-icon tf-icons fa-solid fa-rectangle-list"></i>
+                                <span class="fw-medium d-block mb-1">Total Mata Kuliah</span>
+                            </div>
+                            <div class="dropdown">
+                                <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    <i class="bx bx-dots-vertical-rounded"></i>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                                    <a class="dropdown-item" href="{{ route('dashboard.mata-kuliah.index') }}">Lihat
+                                        Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                        <h3 class="card-title mb-0 mt-3">{{ $totalCourses }}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-6 mb-2">
+                <div class="card p-1">
+                    <div class="card-body">
+                        <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="d-flex align-items-center">
+                                <i class="menu-icon tf-icons fa-solid fa-list-check"></i>
+                                <span class="fw-medium d-block mb-1">Total Mata Kuliah Yang Telah Dilaporkan</span>
+                            </div>
+                            <div class="dropdown">
+                                <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    <i class="bx bx-dots-vertical-rounded"></i>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                                    <a class="dropdown-item" href="{{ route('dashboard.pelaporan-admin.index') }}">Lihat Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                        <h3 class="card-title mb-0 mt-3">{{ $totalCoursesReported }}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-6 mb-2">
+                <div class="card p-1">
+                    <div class="card-body">
+                        <div class="card-title d-flex align-items-start justify-content-between">
+                            <div class="d-flex align-items-center">
+                                <i class="menu-icon tf-icons fa-solid fa-list"></i>
+                                <span class="fw-medium d-block mb-1">Total Mata Kuliah Yang Belum Dilaporkan</span>
+                            </div>
+                            <div class="dropdown">
+                                <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    <i class="bx bx-dots-vertical-rounded"></i>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                                    <a class="dropdown-item" href="{{ route('dashboard.pelaporan-admin.index') }}">Lihat Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                        <h3 class="card-title mb-0 mt-3">{{ $totalCoursesNotReported }}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-dashboard-layout>
