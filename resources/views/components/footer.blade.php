8613c104e63c0ee8b39ecee7baf65ca846e3e3e8
Hizkia Reppi <hizkiajefren0501@gmail.com>
2024-11-07 21:22:34 +0800

    refactor: change footer text

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,16 @@
 <footer class="content-footer footer bg-footer-theme">
     <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
         <div class="mb-2 mb-md-0" style="font-size: 12px;">
-            ©
+            &copy; 2024
             <script>
-                document.write(new Date().getFullYear());
+                if (new Date().getFullYear() > 2024) {
+                    document.write('-' + new Date().getFullYear());
+                }
             </script>
-            - Made with ❤️ by
-            <a href="https://github.com/HizkiaReppi" target="_blank" class="footer-link fw-medium">Hizkia Reppi</a>
+            | Dibuat oleh 
+            <strong>
+                <a href="https://laboratoriumweb.id/#portfolio" target="_blank" class="footer-link fw-bold">Laboratorium Web</a>
+            </strong>
         </div>
     </div>
 </footer>
