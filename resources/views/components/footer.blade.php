4b40d11cda0c840d1732c70f573769e16e5c31dd
Hizkia Reppi <hizkiajefren0501@gmail.com>
2024-11-17 20:05:18 +0800

    refactor: change footer text

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -7,9 +7,13 @@
                     document.write('-' + new Date().getFullYear());
                 }
             </script>
-            | Dibuat oleh 
+            | Dikembangkan oleh 
             <strong>
                 <a href="https://laboratoriumweb.id/#portfolio" target="_blank" class="footer-link fw-bold">Laboratorium Web</a>
+            </strong>
+            bekerja sama dengan
+            <strong>
+                <a href="https://ptik.unima.ac.id" target="_blank" class="footer-link fw-bold">Lab RPL PTIK</a>
             </strong>
         </div>
     </div>
