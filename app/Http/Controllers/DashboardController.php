5c61300b02877dd2595552d82ba09a676818c8fe
Hizkia Reppi <hizkiajefren0501@gmail.com>
2024-12-27 21:51:58 +0800

    feat: add prefix "Fakultas" in chart label

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -24,12 +24,15 @@ class DashboardController extends Controller
             abort(403);
         }
 
+        $currentPeriod = PeriodHelper::getCurrentPeriod();
+
         $totalDepartmentsWithAccounts = User::has('prodi')->count();
-        $totalCourses = Course::where('period_id', PeriodHelper::getCurrentPeriod())->count();
+        $totalCourses = Course::where('period_id', $currentPeriod)->count();
         $totalCoursesReported = $this->getTotalCoursesReported();
         $totalCoursesNotReported = $this->getTotalCoursesNotReported();
 
         $submissionFacultiesData = IKU7::whereNot('status_verifikasi', 'draft')
+            ->where('period_id', $currentPeriod)
             ->get()
             ->groupBy(function ($iku) {
                 return $iku->mataKuliah->prodi->fakultas->name;
@@ -38,11 +41,17 @@ class DashboardController extends Controller
                 return $items->count();
             });
             
-        $verificationStatus = IKU7::select('status_verifikasi')->selectRaw('COUNT(*) as total')->groupBy('status_verifikasi')->get();
-            
+        $verificationStatus = IKU7::select('status_verifikasi')
+            ->selectRaw('COUNT(*) as total')
+            ->where('period_id', $currentPeriod)
+            ->groupBy('status_verifikasi')
+            ->get();
+
         $scoreTrends = Period::with([
-            'pelaporanIku' => function ($query) {
-                $query->selectRaw('period_id, AVG(score_case_method + score_project_based + score_cognitive_task + score_cognitive_quiz + score_cognitive_uts + score_cognitive_uas) as avg_score')->groupBy('period_id');
+            'pelaporanIku' => function ($query) use ($currentPeriod) {
+                $query->where('period_id', $currentPeriod)
+                    ->selectRaw('period_id, AVG(score_case_method + score_project_based + score_cognitive_task + score_cognitive_quiz + score_cognitive_uts + score_cognitive_uas) as avg_score')
+                    ->groupBy('period_id');
             },
         ])->get();
 
@@ -53,10 +62,11 @@ class DashboardController extends Controller
 
     public function getSubmissionsByDepartment(Request $request)
     {
+        $currentPeriod = PeriodHelper::getCurrentPeriod();
         $facultyId = $request->input('faculty_id', Faculty::where('name', 'Teknik')->first()->id);
 
-        $submissionData = IKU7::whereHas('mataKuliah.prodi', function ($query) use ($facultyId) {
-            $query->where('faculty_id', $facultyId);
+        $submissionData = IKU7::whereHas('mataKuliah.prodi', function ($query) use ($facultyId, $currentPeriod) {
+            $query->where('faculty_id', $facultyId)->where('period_id', $currentPeriod);
         })
             ->whereNot('status_verifikasi', 'draft')
             ->with('mataKuliah.prodi')
@@ -73,7 +83,9 @@ class DashboardController extends Controller
 
     public function getIKUDataByFaculty()
     {
+        $currentPeriod = PeriodHelper::getCurrentPeriod();
         $ikuData = IKU7::with('mataKuliah.prodi.fakultas')
+            ->where('period_id', $currentPeriod)
             ->get()
             ->groupBy(function ($iku) {
                 return $iku->mataKuliah->prodi->fakultas->name;
@@ -93,10 +105,11 @@ class DashboardController extends Controller
 
     public function getIKUDataByDepartment(Request $request)
     {
+        $currentPeriod = PeriodHelper::getCurrentPeriod();
         $facultyId = $request->input('faculty_id', Faculty::first()->id);
 
-        $ikuData = IKU7::whereHas('mataKuliah.prodi', function ($query) use ($facultyId) {
-            $query->where('faculty_id', $facultyId);
+        $ikuData = IKU7::whereHas('mataKuliah.prodi', function ($query) use ($facultyId, $currentPeriod) {
+            $query->where('faculty_id', $facultyId)->where('period_id', $currentPeriod);
         })
             ->with('mataKuliah.prodi')
             ->get()
